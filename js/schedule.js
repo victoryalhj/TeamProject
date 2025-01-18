e2fa66e1b1db2e2ea7a2eb9e53ebd3247540da17
victoryalhj <victoryalhj@gmail.com>
2025-01-18 13:31:34 +0000

    navbar changed

--- js/schedule.js
+++ js/schedule.js
@@ -1,9 +1,9 @@
 function scrollLeftHandler(day) {
   var gameInfo = document.getElementById('game-info-container-' + day);
-  gameInfo.scrollLeft -= 200; // Scroll left by 200px
+  gameInfo.scrollLeft -= 200; 
 }
 
 function scrollRightHandler(day) {
   var gameInfo = document.getElementById('game-info-container-' + day);
-  gameInfo.scrollLeft += 200; // Scroll right by 200px
+  gameInfo.scrollLeft += 200; 
 }
